e992daa2080aaeb881e81f63acfae3bb0d6dd28d
Ahmadoubambamb <mboup.ahmadou-bamba@ugb.edu.sn>
2025-04-26 16:12:06 +0000

    exo3

--- LesCodeC_Algo1/exo3/main.c
+++ LesCodeC_Algo1/exo3/main.c
@@ -2,27 +2,28 @@
 #include <stdlib.h>
 /* exo3: un entier est dit distinct s'il est composer de chiffre tous differents*/
 int main(){
-    int i , est_distinct , tmp1 , tmp2 , rest, j = 0;
+    int i , est_distinct = 1 , tmp1 , tmp2 , rest, j = 0;
     for(i = 1000 ; i < 2000 ; i++){
        tmp1 = i ;
         while(tmp1 != 0){
-             tmp2 = i;
-             est_distinct = 0;
+            est_distinct = 1;
+             tmp2 = tmp1/10;
             rest = tmp1%10;
             while(tmp2 !=0){
                 if(rest == tmp2%10)
-                est_distinct++ ;
-                   if(est_distinct >= 2)
+                 { est_distinct = 0;
                        break;
+                    }
                 tmp2 /= 10;
             }
-            if(est_distinct >= 2)
+            if(est_distinct == 0)
                 break;
             tmp1 /=10;
         }
-        if(est_distinct == 1)
+        if(est_distinct){
             printf("%d  ",i);
-        if(j++%20 == 0) printf("\n");
+            //pour que ca affiche 20 et passe a la ligne
+        if(j++%20 == 0) printf("\n");}
     }
     return 0;
 }
